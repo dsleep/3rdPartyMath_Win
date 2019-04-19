f22160564f860daf6771e660884de7bd12eb06b3
David Sleeper <david.sleeper@gmail.com>
2019-04-18 23:54:36 -0400

    - fixes

--- libxml2/preparebuild.ps1
+++ libxml2/preparebuild.ps1
@@ -1,4 +1,4 @@
 $OutputPath = $3rdPartyPath + "\" + $CurrentModule
 Write-Host "BUILDING MODULE: " $ModulePath "OUTPUT TO" $OutputPath
 # run the process
-Start-Process $CMakePath -ArgumentList "-G `"$CMakeVSString`" -BbuildVS2017 -Hsource -DCMAKE_INSTALL_PREFIX=`"$OutputPath`" -DCMAKE_INSTALL_LIBDIR=`"$CMakeLibInstall`" " -Wait
+Start-Process $CMakePath -ArgumentList "-G `"$CMakeVSString`" -BbuildVS2017 -Hsource -DCMAKE_INSTALL_PREFIX=`"$OutputPath`" -DCMAKE_INSTALL_LIBDIR=`"$CMakeLibInstall`" -DLIBICONV_INCLUDE_DIR=`"$3rdPartyForwardPath/libiconv/include`" -DLIBICONV_LIBRARY=`"$3rdPartyForwardPath/libiconv/$CMakeLibInstall`" " -Wait
