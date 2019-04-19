9ff475db5a097d6afe26180913d0913d95c15779
David Sleeper <david.sleeper@gmail.com>
2019-04-18 23:55:24 -0400

    - ceres fixes

--- ceres/preparebuild.ps1
+++ ceres/preparebuild.ps1
@@ -2,4 +2,4 @@ $OutputPath = $3rdPartyPath + "\" + $CurrentModule
 Write-Host "MODULE CMAKE MODULE: " $ModulePath "OUTPUT TO" $OutputPath
 
 # run the process
-Start-Process $CMakePath -ArgumentList "-G `"$CMakeVSString`" -B$VSMakeBuildFolder -Hsource -DCMAKE_INSTALL_PREFIX=`"$OutputPath`" -DCMAKE_INSTALL_LIBDIR=`"$CMakeLibInstall`" -DBUILD_TESTING=false" -Wait
+Start-Process $CMakePath -ArgumentList "-G `"$CMakeVSString`" -B$VSMakeBuildFolder -Hsource -DCMAKE_INSTALL_PREFIX=`"$OutputPath`" -DCMAKE_INSTALL_LIBDIR=`"$CMakeLibInstall`" -DBUILD_EXAMPLES=false -DBUILD_TESTING=false -DSUITESPARSE=true -DSuiteSparse_DIR=`"$3rdPartyForwardPath/suitesparse`" -DGLOG_INCLUDE_DIR_HINTS=`"$3rdPartyForwardPath/glog/include`" -DGLOG_LIBRARY_DIR_HINTS=`"$3rdPartyForwardPath/glog/$CMakeLibInstall`" -DEigen3_DIR=`"$3rdPartyForwardPath/eigen`" " -Wait
