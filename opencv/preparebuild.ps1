e065a8060e9da01e280c93f9373fdbe5e0ee7ae5
David Sleeper <david.sleeper@gmail.com>
2019-04-19 12:22:48 -0400

    - don't build tests

--- opencv/preparebuild.ps1
+++ opencv/preparebuild.ps1
@@ -1,4 +1,4 @@
 $OutputPath = $3rdPartyPath + "\" + $CurrentModule
 Write-Host "BUILDING MODULE: " $ModulePath "OUTPUT TO" $OutputPath
 # run the process
-Start-Process $CMakePath -ArgumentList "-G `"$CMakeVSString`" -B$VSMakeBuildFolder -Hsource -DCMAKE_INSTALL_PREFIX=`"$OutputPath`" -DCMAKE_INSTALL_LIBDIR=`"$CMakeLibInstall`" " -Wait
+Start-Process $CMakePath -ArgumentList "-G `"$CMakeVSString`" -B$VSMakeBuildFolder -Hsource -DCMAKE_INSTALL_PREFIX=`"$OutputPath`" -DCMAKE_INSTALL_LIBDIR=`"$CMakeLibInstall`" -DBUILD_TESTS=false -DBUILD_PERF_TESTS=false " -Wait
