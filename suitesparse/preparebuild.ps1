4fa1efb38a7753dfc025813567dfb2a7383ec3e3
David Sleeper <david.sleeper@gmail.com>
2019-04-19 11:47:40 -0400

    -2015 fixes

--- suitesparse/preparebuild.ps1
+++ suitesparse/preparebuild.ps1
@@ -1,4 +1,4 @@
 $OutputPath = $3rdPartyPath + "\" + $CurrentModule
 Write-Host "BUILDING MODULE: " $ModulePath "OUTPUT TO" $OutputPath
 # run the process
-Start-Process $CMakePath -ArgumentList "-G `"$CMakeVSString`" -BbuildVS2017 -Hsource -DCMAKE_INSTALL_PREFIX=`"$OutputPath`" -DCMAKE_INSTALL_LIBDIR=`"$CMakeLibInstall`" -DBUILD_METIS=false" -Wait
+Start-Process $CMakePath -ArgumentList "-G `"$CMakeVSString`" -B$VSMakeBuildFolder -Hsource -DCMAKE_INSTALL_PREFIX=`"$OutputPath`" -DCMAKE_INSTALL_LIBDIR=`"$CMakeLibInstall`" -DBUILD_METIS=false" -Wait
