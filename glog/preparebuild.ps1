ba9d56edb17372add2428ec71cd68e27c316c313
David Sleeper <david.sleeper@gmail.com>
2019-04-23 12:50:43 -0400

    - gflag fixes

--- glog/preparebuild.ps1
+++ glog/preparebuild.ps1
@@ -1,4 +1,4 @@
 $OutputPath = $3rdPartyPath + "\" + $CurrentModule
 Write-Host "BUILDING MODULE: " $ModulePath "OUTPUT TO" $OutputPath
 # run the process
-Start-Process $CMakePath -ArgumentList "-G `"$CMakeVSString`" -B$VSMakeBuildFolder -Hsource -DCMAKE_INSTALL_PREFIX=`"$OutputPath`" -DCMAKE_INSTALL_LIBDIR=`"$CMakeLibInstall`" " -Wait
+Start-Process $CMakePath -ArgumentList "-G `"$CMakeVSString`" -B$VSMakeBuildFolder -Hsource -DCMAKE_INSTALL_PREFIX=`"$OutputPath`" -DCMAKE_INSTALL_LIBDIR=`"$CMakeLibInstall`" -DBUILD_TESTING=False -DSuiteSparse_DIR=`"$3rdPartyForwardPath/gflags`" " -Wait
